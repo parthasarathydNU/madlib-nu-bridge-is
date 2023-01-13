46286a97c5950b78f27256d75d7b0540c7171e62
fraander <57777918+fraander@users.noreply.github.com>
2023-01-13 13:19:00 -0500

    Output display fixes

--- src/Output.js
+++ src/Output.js
@@ -5,13 +5,11 @@ export default function Output({ word1, word2, word3, word4, word5 }) {
 
       <p>
         Welcome to Oasis, the ultimate destination for computer science majors
-        at Northeastern! As a member, you'll have access {word1} events,
-        {word2} opportunities to network, and the chance to meet others who
-        share your passion for {word3}. Whether you're looking to improve your
-        coding skills, learn a new technology, or just want to {word4} with
+        at Northeastern! As a member, you'll have access <b>{word1}</b> events, <b>{word2}</b> opportunities to network, and the chance to meet others who
+        share your passion for <b>{word3}</b>. Whether you're looking to improve your
+        coding skills, learn a new technology, or just want to <b>{word4}</b> with
         other CS enthusiasts, Oasis is the perfect place for you. So join us,
-        and let's
-        {word5} together!
+        and let's <b>{word5}</b> together!
       </p>
     </div>
   );
