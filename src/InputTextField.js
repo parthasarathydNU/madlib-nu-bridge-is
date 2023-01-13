7bccd88da027b2c83bfa1689d3392d84b7e3176c
Jay Sella <jay@jaysella.dev>
2023-01-13 16:15:11 -0500

    fix htmlFor bug

--- src/InputTextField.js
+++ src/InputTextField.js
@@ -1,25 +1,23 @@
-import React from "react";
+import React, { useId } from "react";
 
 export function InputTextField({ get, set, partOfSpeech }) {
+  const fieldId = `${partOfSpeech}-${useId()}`;
+
   return (
-    <div>
-      <ul>
-        <label for="text-input">
-          {partOfSpeech.charAt(0).toUpperCase() + partOfSpeech.slice(1) + ": "}
-        </label>
+    <div className="input-group">
+      <label htmlFor={fieldId}>
+        {partOfSpeech.charAt(0).toUpperCase() + partOfSpeech.slice(1) + ": "}
+      </label>
 
-        <br />
-
-        <input
-          id="text-input"
-          type="text"
-          value={get}
-          placeholder={partOfSpeech}
-          onChange={(e) => {
-            set(e.target.value);
-          }}
-        />
-      </ul>
+      <input
+        id={fieldId}
+        type="text"
+        value={get}
+        placeholder={partOfSpeech}
+        onChange={(e) => {
+          set(e.target.value);
+        }}
+      />
     </div>
   );
 }
