04c4bc85fdce733b5314ef40f0f73570bb861f68
Jay Sella <jay@jaysella.dev>
2023-01-13 16:02:39 -0500

    fix responsive styling

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   return (
     <div className="App">
       <img
-        style={{ display: "block", height: "10rem", margin: "0 auto" }}
+        className="logo"
         src="http://www.9yahds.com/content/uploads/2015/05/midlib3.png"
         alt="Mad Libs"
       />
